364e80ea36b4cbb68e2d50e10f367b6bdad30af6
dohtiev <e.dohtiev@gmail.com>
2024-07-09 21:07:34 +0300

    add gitignore file

--- js/recipe.js
+++ js/recipe.js
@@ -20,6 +20,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('recipe-servings').textContent = recipe.servings;
     document.getElementById('recipe-instructions').textContent = recipe.instructions;
+
+    // Check if the recipe is already in favorites
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const isFavorite = favorites.some(favRecipe => favRecipe.title === recipe.title);
+
+    const favoriteButton = document.getElementById('favorite-button');
+    if (isFavorite) {
+        favoriteButton.innerHTML = '<i class="fas fa-heart"></i> Remove from Favorites';
+    } else {
+        favoriteButton.innerHTML = '<i class="fas fa-heart"></i> Add to Favorites';
+    }
+
+    favoriteButton.addEventListener('click', () => {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        const isFavorite = favorites.some(favRecipe => favRecipe.title === recipe.title);
+
+        if (isFavorite) {
+            const updatedFavorites = favorites.filter(favRecipe => favRecipe.title !== recipe.title);
+            localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+            favoriteButton.innerHTML = '<i class="fas fa-heart"></i> Add to Favorites';
+        } else {
+            favorites.push(recipe);
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+            favoriteButton.innerHTML = '<i class="fas fa-heart"></i> Remove from Favorites';
+        }
+    });
 });
 
 function copyLink() {
--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,21 @@ document.getElementById('search-form').addEventListener('submit', async (event) 
     event.preventDefault();
     const query = document.getElementById('search-input').value;
     const recipeList = document.getElementById('recipe-list');
+    const popularRecipes = document.getElementById('popular-recipes');
+    const popularSection = document.getElementById('popular-section');
     const noRecipesMessage = document.getElementById('no-recipes');
-
+    const favoritesSection = document.getElementById('favorites-section');
     try {
         const response = await fetch(`https://api.api-ninjas.com/v1/recipe?query=${query}`, {
-            headers: { 'X-Api-Key': 'YW1hDeolgH2EW3yRtEGiDw==shmwg9WZvwAj6PBT' }
+            headers: { 'X-Api-Key': window.API_KEY }
         });
         const data = await response.json();
 
         recipeList.innerHTML = '';
         if (data.length > 0) {
+            popularSection.style.display = 'none';
+            favoritesSection.style.display = 'none';
+            recipeList.style.display = 'block';
             noRecipesMessage.style.display = 'none';
             data.forEach(recipe => {
                 const listItem = document.createElement('li');
@@ -24,9 +29,40 @@ document.getElementById('search-form').addEventListener('submit', async (event) 
                 recipeList.appendChild(listItem);
             });
         } else {
+            recipeList.style.display = 'none';
             noRecipesMessage.style.display = 'block';
+            popularSection.style.display = 'none';
+            favoritesSection.style.display = 'none';
         }
     } catch (error) {
         console.error('Error fetching recipes:', error);
+        recipeList.style.display = 'none';
+        popularSection.style.display = 'none';
+        noRecipesMessage.style.display = 'block';
+        favoritesSection.style.display = 'none';
     }
 });
+
+function loadFavorites() {
+    const favoritesSection = document.getElementById('favorites-section');
+    const favoritesList = document.getElementById('favorites-recipes');
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    favoritesList.innerHTML = '';
+    if (favorites.length > 0) {
+        favorites.forEach(recipe => {
+            const listItem = document.createElement('li');
+            listItem.classList.add('recipe-item');
+            const link = document.createElement('a');
+            link.href = `recipe.html?recipe=${encodeURIComponent(JSON.stringify(recipe))}`;
+            link.classList.add('recipe-link');
+            link.textContent = `${recipe.title} (${recipe.servings})`;
+            listItem.appendChild(link);
+            favoritesList.appendChild(listItem);
+        });
+    } else {
+        favoritesSection.style.display = 'none';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadFavorites);
